488b6bc9095f19cb12132515a16ac9a0d46050cc
ayadnez <zahid.ahmad.0303@gmail.com>
2024-08-16 02:16:43 +0530

    added menu creation-search functionality

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -142,11 +142,169 @@ const getCategoryById = async (req, res) => {
     }
 }
 
+const getAllSubCategories = async(req,res) => {
+    try {
+        // Fetch all categories
+        const categories = await Category.find({}); // Use an empty object to fetch all categories
+
+        // Extract subcategories from each category
+        const subCategories = categories.flatMap(category => category.subCategories);
+
+        res.status(200).json(subCategories)
+    } catch(error) {
+        console.error('error while fetching subcategories',error)
+        res.status(500).json({message:'server error',error})
+    }
+}
+
+const subCategories = async (req, res) => {
+    try{
+        const {categoryId} = req.params
+        const  category = await Category.findById(categoryId)
+
+        if(!category) {
+            return res.status(404).json({message : "category not found"})
+        }
+
+        res.status(200).json(category.subCategories)
+    } catch(error) {
+        console.error('error while fetching sub categories ', error)
+        res.status(500).json({message :'server error', error})
+    }
+}
+
+const getSubCategoryById = async(req,res) => {
+    try {
+        const {id } = req.params 
+
+        const category = await Category.findOne({ 'subCategories._id': id });  
+
+        if (!category) {
+            return res.status(404).json({ message: 'Subcategory not found' });
+        }
+
+        // Find the specific subcategory by ID within the subCategories array
+        const subCategory = category.subCategories.id(id);
+
+
+        res.status(200).json(subCategory)
+
+    } catch(error) {
+        console.error('error while fetching sub category by id', error)
+        res.status(500).json({message :"server error ",error})
+    }
+}
+
+const getAllItems = async (req, res) => {
+
+    try {
+        const categories = await Category.find({})
+
+       // Extract items from each subcategory within all categories
+        const items = categories.flatMap(category =>
+            category.subCategories.flatMap(subCategory => subCategory.items)
+        );
+
+        if(items.length===0) {
+            return res.status(404).json({message :"no items found"})
+        }
+
+        res.status(200).json(items)
+    } catch(error) {
+        console.error("error while fetching items",error)
+        res.status(500).json({message : "server error", error})
+    }
+}
+
+const getAllItemsByCategoryId = async (req, res) => {
+    try {
+        const {id} = req.params 
+        const category = await Category.findById(id)
+
+         // Extract items from each subcategory within the category
+         const items = category.subCategories.flatMap(subCategory => subCategory.items);
+
+         if (items.length === 0) {
+             return res.status(404).json({ message: 'No items found in this category' });
+         }
+
+        res.status(200).json(items)
+    }catch(error) {
+        console.error('error while fetching items for the given category',error)
+        res.status(500).json({message : "server error",error})
+    }
+}
+
+const getAllItemsOfSubCategory = async (req, res) => {
+    try {
+        const {categoryId,subCategoryId} = req.params 
+
+         // Find the category by ID
+         const category = await Category.findById(categoryId);
+         if (!category) {
+             return res.status(404).json({ message: 'Category not found' });
+         }
+ 
+         // Find the subcategory by ID within the category
+         const subCategory = category.subCategories.id(subCategoryId);
+         if (!subCategory) {
+             return res.status(404).json({ message: 'Subcategory not found' });
+         }
+
+         // all items of the subcategory 
+         const items = subCategory.items
+
+         if (items.length === 0) {
+            return res.status(404).json({message : "no items found in this subcategory"})
+         }
+
+         res.status(200).json(items)
+    } catch(error) {
+        console.error("error while fetching the items", error)
+        res.status(500).json({message : "server error ", error})
+    }
+}
+
+const getItemById = async (req, res) => {
+    try {
+        const {id} = req.params
+
+       // Find the category that contains the item within its subcategories
+       const category = await Category.findOne({ 'subCategories.items._id': id });
+
+       if (!category) {
+           return res.status(404).json({ message: 'Item not found' });
+       }
+
+       // Find the subcategory that contains the item
+       let item;
+       for (let subCategory of category.subCategories) {
+           item = subCategory.items.id(id);
+           if (item) break;
+       }
+
+       if (!item) {
+           return res.status(404).json({ message: 'Item not found' });
+       }
+
+    } catch(error) {
+        console.error('error while fetching the item',error)
+        res.status(500).json({message : "server error ", error})
+    }
+}
+
 
 module.exports = {
     createCategory,
     createSubCategory,
     createItem,
     getAllCategories,
-    getCategoryById
+    getCategoryById,
+    getAllSubCategories,
+    subCategories,
+    getSubCategoryById,
+    getAllItems,
+    getAllItemsByCategoryId,
+    getAllItemsOfSubCategory,
+    getItemById
 }
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,7 +1,17 @@
 const router = require('express').Router()
-const { createCategory, createSubCategory ,createItem,
+const { createCategory, 
+    createSubCategory ,
+    createItem,
     getAllCategories,
-    getCategoryById
+    getCategoryById,
+    getAllSubCategories,
+    subCategories,
+    getSubCategoryById,
+    getAllItems,
+    getAllItemsByCategoryId,
+    getAllItemsOfSubCategory,
+    getItemById
+
 } = require('../controllers/user.controller')
 
 
@@ -9,7 +19,19 @@ router.post('/createCategory',createCategory)
 router.post('/subcategory/:categoryId',createSubCategory)
 router.post('/createItem/:categoryId/:subCategoryId',createItem)
 
+// get categories routes
 router.get('/categories',getAllCategories)
 router.get('/categories/:id',getCategoryById)
+
+// get subcategories routes
+router.get('/subcategories',getAllSubCategories)
+router.get('/categories/:categoryId/subcategories',subCategories)
+router.get('/subcategory/:id',getSubCategoryById)
+
+// get items routes 
+router.get('/items',getAllItems)
+router.get('/items/:id', getAllItemsByCategoryId)
+router.get('/items/:categoryId/:subCategoryId',getAllItemsOfSubCategory)
+router.get('/item/:id',getItemById)
 
 module.exports = router;
